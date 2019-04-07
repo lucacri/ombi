71b4cbc7f7ce1ba726a09504f788dc66f4e5186c
Luca Critelli <lucacri@gmail.com>
2019-04-07 11:47:22 -0400

    Health check with http too

--- root/health-check.sh
+++ root/health-check.sh
@@ -3,9 +3,9 @@ LAST_LOG=`ls -Art /config/Logs | tail -n 1`
 
 if grep -q 'database is locked' /config/Logs/$LAST_LOG; then
     echo "Gotta restart this"
-    echo "" >  /config/Logs/$LAST_LOG
-    rm /config/Schedules.db
     exit 1
 fi
+
+timeout 3s curl -fIsS http://localhost:3579 > /dev/null
 
 exit 0
